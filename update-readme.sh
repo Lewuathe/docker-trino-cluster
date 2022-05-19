8f30ba167aa883d22e0b313d4d5b6a1021001179
Satya Kommula <satyakommula@ymail.com>
2022-05-19 23:48:10 +0530

    username and paswd as args for update_readme.sh

--- update-readme.sh
+++ update-readme.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 docker run -v $PWD:/workspace \
-    -e DOCKERHUB_USERNAME=${DOCKER_USER} \
-    -e DOCKERHUB_PASSWORD=${DOCKER_PASSWORD} \
+    -e DOCKERHUB_USERNAME=$1 \
+    -e DOCKERHUB_PASSWORD=$2 \
     -e DOCKERHUB_REPOSITORY='satyakommula/trino-coordinator' \
     -e README_FILEPATH='/workspace/README.md' \
     peterevans/dockerhub-description:latest
 docker run -v $PWD:/workspace \
-    -e DOCKERHUB_USERNAME=${DOCKER_USER} \
-    -e DOCKERHUB_PASSWORD=${DOCKER_PASSWORD} \
+    -e DOCKERHUB_USERNAME=$1 \
+    -e DOCKERHUB_PASSWORD=$2 \
     -e DOCKERHUB_REPOSITORY='satyakommula/trino-worker' \
     -e README_FILEPATH='/workspace/README.md' \
     peterevans/dockerhub-description:latest
